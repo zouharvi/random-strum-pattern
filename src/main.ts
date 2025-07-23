bff114b2b1c54930b0d266df347f3712736470b9
William Haddad Monteiro de Castro Fraga Gomes <whaddad@ciandt.com>
2025-07-23 16:36:43 -0300

    Added visual metronome

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,49 @@
+let metronomeTimerId: number | null = null;
+let isPlaying = false;
+let currentBeat = 0;
+
+function playMetronome() {
+    if (isPlaying) return;
+
+    const bpmValue = $("#bpm").val() || '120';
+    const bpm = parseInt(String(bpmValue), 10);
+
+    if (isNaN(bpm) || bpm <= 0) {
+        alert("Please enter a valid BPM.");
+        return;
+    }
+
+    isPlaying = true;
+    currentBeat = 0;
+
+    const interval = (60 * 1000) / (bpm * 2);
+    function tick() {
+        const totalBeats = ($("#beats_in_bar").val() as number) * 2;
+        
+        $("#area_output span").removeClass("highlight");
+        $(`#beat-${currentBeat}`).addClass("highlight");
+
+        currentBeat = (currentBeat + 1) % totalBeats;
+    }
+
+    tick();
+    metronomeTimerId = window.setInterval(tick, interval);
+}
+
+function stopMetronome() {
+    if (!isPlaying) return;
+    
+    if (metronomeTimerId !== null) {
+        window.clearInterval(metronomeTimerId);
+    }
+
+    metronomeTimerId = null;
+    isPlaying = false;
+    currentBeat = 0;
+
+    $("#area_output span").removeClass("highlight");
+}
+
 function shuffle(a: any[]) {
     // taken from https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
     for (let i = a.length - 1; i > 0; i--) {
@@ -12,14 +58,18 @@ $("#beats_in_bar").on("input", generate);
 $("#total_strums").on("input", generate);
 $("#fix_first").on("input", generate);
 $("#whitespace_fill").on("input", generate);
+$("#play_button").on("click", playMetronome);
+$("#stop_button").on("click", stopMetronome);
 
 function generate() {
+    stopMetronome();
+
     let bib = $("#beats_in_bar").val() as number;
     let tts = $("#total_strums").val() as number;
-    $("#total_strums").attr("max", 2*bib);
-    if (tts > 2*bib) {
-        $("#total_strums").val(2*bib);
-        tts = 2*bib;
+    $("#total_strums").attr("max", 2 * bib);
+    if (tts > 2 * bib) {
+        $("#total_strums").val(2 * bib);
+        tts = 2 * bib;
     }
 
     let fix_first = $("#fix_first").is(":checked") as boolean;
@@ -30,26 +80,26 @@ function generate() {
         tts -= 1;
     }
 
-    let pattern_sig : string[] = [];
+    let pattern_sig: string[] = [];
     for (let i = 0; i < bib; i++) {
-        pattern_sig.push((i+1).toString());
+        pattern_sig.push((i + 1).toString());
         pattern_sig.push("+");
     }
 
-    let pattern_all : any[] = [];
+    let pattern_all: any[] = [];
     for (let i = 0; i < bib; i++) {
         if (i != 0 || !fix_first) {
-            // don't add if we're fixing the first downstream
-            pattern_all.push([2*i, "↓"]);
+            pattern_all.push([2 * i, "↓"]);
         }
-        pattern_all.push([2*i+1, "↑"]);
+        pattern_all.push([2 * i + 1, "↑"]);
     }
 
     shuffle(pattern_all);
-    let whitespaceChar = $("#whitespace_fill").val() || "&nbsp;";
-    let pattern_final : string[] = Array(bib*2).fill(whitespaceChar);
+    
+    let whitespaceChar = ($("#whitespace_fill").val() as string) || "&nbsp;";
+    let pattern_final: string[] = Array(bib * 2).fill(whitespaceChar);
+    
     for (let i = 0; i < tts; i++) {
-        console.log(pattern_all[i]);
         pattern_final[pattern_all[i][0]] = pattern_all[i][1];
     }
 
@@ -57,7 +107,11 @@ function generate() {
         pattern_final[0] = "↓";
     }
 
-    $("#area_output").html(pattern_sig.join("") + "<br>" + pattern_final.join(""));
+    const patternHtml = pattern_final.map((char, index) => {
+        return `<span id="beat-${index}">${char}</span>`;
+    }).join("");
+
+    $("#area_output").html(pattern_sig.join("") + "<br>" + patternHtml);
 }
 
 generate();
